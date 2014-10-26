71d5a835e9b1a40d824f9421b2e27a07b98cfe3c
xapantu <xapantu@gmail.com>
2014-10-27 00:07:54 +0100

    Code refactor, add sequelize in the modules

--- imap-server.cs
+++ imap-server.cs
@@ -2,6 +2,25 @@ net = require 'net'
 imap = require './imap.js'
 starttls = require './starttls.js'
 
+# cmd: an array of an imap command, e.g. ['aa0', 'login', 'username', 'password']
+# where aa0 is the id of the request, and must be sent when the response is complete
+# assume cmd is correct
+imap_reply =
+	end : (cmd) ->
+		cmd[0] + " OK " + cmd[1].toUpperCase() + " completed\r\n"
+	more : (cmd) ->
+		"+\r\n"
+	capability: (cmd, ssl) ->
+		if ssl
+			'* CAPABILITY IMAP4rev1 AUTH=EXTERNAL\r\n'
+		else
+			'* CAPABILITY IMAP4rev1 STARTTLS LOGINDISABLED\r\n'
+	ok: () ->
+		'* OK\r\n'
+	
+	bad: (cmd) ->
+		cmd[0] + " BAD " + cmd[1].toUpperCase() + "\r\n"
+
 
 server = net.createServer (c) -> #'connection' listener
 	console.log 'server connected'
@@ -9,24 +28,18 @@ server = net.createServer (c) -> #'connection' listener
 	wait_for_more = null
 	must_quit = false
 	ssl = false
+	logged = false
+	username = false
 
+	# cmd: an array of an imap command, e.g. ['aa0', 'login', 'username', 'password']
+	# where aa0 is the id of the request, and must be sent when the response is complete
 	handle_command = (cmd) ->
 		if cmd.length < 2
 			"* You have to handle uids of the queries…"
 		else
 			switch cmd[1].toLowerCase()
 				when 'capability'
-					if c.ssl
-						'* CAPABILITY IMAP4rev1 AUTH=EXTERNAL\r\n' +
-						cmd[0] + " OK CAPABILITY completed\r\n"
-					else
-						'* CAPABILITY IMAP4rev1 STARTTLS LOGINDISABLED\r\n' +
-						cmd[0] + " OK CAPABILITY completed\r\n"
-				when 'authenticate', 'AUTHENTICATE'
-					wait_for_auth = (e) ->
-						[cmd[0] + " OK AUTHENTICATE completed\r\n", '']
-					wait_for_more = wait_for_auth
-					'+\n\r'
+					(imap_reply.capability cmd, c.ssl) + (imap_reply.end cmd)
 				when 'logout'
 					must_quit = true
 					null
@@ -36,25 +49,26 @@ server = net.createServer (c) -> #'connection' listener
 					if cmd.length != 4
 						"BAD login command"
 					else
-						cmd[0] + " OK LOGIN completed\r\n"
+						imap_reply.end cmd
 				when 'list'
-					cmd[0] + " OK list completed\r\n"
+					imap_reply.end cmd
 				when 'lsub'
-					cmd[0] + " OK lsub completed\r\n"
+					imap_reply.end cmd
 				when 'create'
-					cmd[0] + " OK create completed\r\n"
+					imap_reply.end cmd
 				when 'select'
-					cmd[0] + " OK select completed\r\n"
+					imap_reply.end cmd
 				when 'noop'
-					cmd[0] + " OK noop completed\r\n"
-				else '* OK'
+					imap_reply.end cmd
+				else
+					imap_reply.ok()
 
 	
 	c.on 'end', () ->
 		console.log 'server disconnected'
 	
-	#c.on 'error', (e) ->
-	#	console.log e
+	c.on 'error', (e) ->
+		console.log e
 
 	c.on 'data', (e) ->
 		console.log ">>>> Reçu : " + e.toString()
@@ -82,8 +96,11 @@ server = net.createServer (c) -> #'connection' listener
 			c.end()
 
 
-	c.write '* OK\n\r'
-	#c.pipe c
+	# Show we're alive, some clients don't start, event if it is not in the spec (?)
+	c.write imap_reply.ok()
+
+	# What's the use of this thing?
+	# c.pipe c
 
 server.listen 1430, () -> #'listening' listener
 	console.log 'server bound'
